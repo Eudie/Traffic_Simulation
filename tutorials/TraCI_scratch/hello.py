8d769fb9fd29221c1ce030d9beece3c7f44cd4eb
Eudie <utkarsh.dubey.26@gmail.com>
2018-02-12 18:45:16 +0530

    mixed traffic

---
+++ tutorials/TraCI_scratch/hello.py
@@ -0,0 +1,9 @@
+
+import os
+import sys
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
