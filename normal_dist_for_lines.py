be599c18dd0ac4758d9deac01bc6e7244abebc23
Eudie <utkarsh.dubey.26@gmail.com>
2018-07-04 15:52:49 +0530

    started probabilist matching roads from different maps

---
+++ normal_dist_for_lines.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Eudie
+
+"""
+Here I am trying to make probalistic function to find two line segments are actually one.
+This is the extension of normal distribution in zero dimension.
+
+"""
+
+
+class Distribution:
+    """
+    Here we will convert zero dimensional normal distribution to 1 dimension to work for line segments
+    """
+
+    def __init__(self, sigma):
+        self.sigma = sigma
+
+    def probability_line(self, first_seg, second_seg):
+        """
+        Here we will compare one segment to another
+        :param first_seg: coordinates of end points of first segment line [[x1, y1], [x2, y2]]
+        :param second_seg: coordinates of end points of second segment line [[x1, y1], [x2, y2]]
+        :return: float containing prob
+        """
+
+        # TODO
+
+    def probability_polyline(self, first_polyline, second_polyline):
+        """
+        Here we will compare one segment to another
+        :param first_polyline: coordinates of points of first polyline line [[x1, y1], [x2, y2], [x3, y3]]
+        :param second_polyline: coordinates of points of second polyline line [[x1, y1], [x2, y2], [x3, y3]]
+        :return: float containing prob
+        """
+
+        prob = 0
+        for i in range(1, len(first_polyline)):
+            for j in range(1, len(second_polyline)):
+                seg_1 = [first_polyline[i - 1], first_polyline[i]]
+                seg_2 = [second_polyline[j - 1], second_polyline[j]]
+
+                prob += self.probability_line(seg_1, seg_2)
+
+        return prob
--- traffic_from_api.py
+++ traffic_from_api.py
@@ -13,6 +13,7 @@ import datetime
 import defusedxml.ElementTree as Xml
 import sumo_information
 from make_file_names import FileName
+from normal_dist_for_lines import Distribution
 
 
 class HereMapInfo:
@@ -27,6 +28,8 @@ class HereMapInfo:
         self.top = str(top)
 
         self.filename = FileName(self.data_folder)
+        self.map_scale = 0.00001
+        self.normal_dist = Distribution(sigma=2)
 
         with open('heremap_credentials.json') as f:
             api_keys = json.load(f)
@@ -92,18 +95,73 @@ class HereMapInfo:
         df['PATCH'] = df['PC'].map(str) + df['QD']
         return df
 
+    def get_correction_offset(self, sumo_road_points, heremap_road_points):
+        """
+        Here we will try to find the optimum offset my convolving one map over other
+        :param sumo_road_points:
+        :param heremap_road_points:
+        :return: tuple of offset value
+        """
+        max_prob = 0
+        offset = [0, 0]
+        window = 25
+        for i in range(-1*window, window):
+            for j in range(-1*window, window):
+                i *= self.map_scale
+                j *= self.map_scale
+                offseted_sumo_road_points = sumo_road_points.copy()
+                total_prob = 0
+                for k in offseted_sumo_road_points.values():
+                    for l in k:
+                        l[0] += i
+                        l[1] += j
+
+                for m in heremap_road_points.values():
+                    for n in offseted_sumo_road_points.values():
+                        total_prob += self.normal_dist.probability_polyline(first_polyline=n, second_polyline=m)
+
+                if total_prob > max_prob:
+                    offset = [i, j]
+                    max_prob = total_prob
+
+        return offset
+
     def find_mapping(self):
         """
         Here we will find the which road from here map is linked to that of sumo or osm
         :return: dictionary for
         """
 
+        sumo_road_points = {'122516806': [(1, 2), (2, 4)]}
+        heremap_road_points = {'2572+': [(1, 2), (2, 4)]}
+
+        correction_offset = self.get_correction_offset(sumo_road_points, heremap_road_points)
+
+        sumo_names = sumo_road_points.keys()
+        heremap_names = heremap_road_points.keys()
+
+        offseted_sumo_road_points = sumo_road_points.copy()
+        for k in offseted_sumo_road_points.values():
+            for l in k:
+                l[0] += correction_offset[0]
+                l[1] += correction_offset[1]
+
+        mapping = []
+        for i in sumo_names:
+            road_prob = []
+            for j in heremap_names:
+                road_prob.append(self.normal_dist.probability_polyline(first_polyline=offseted_sumo_road_points[i],
+                                                                       second_polyline=heremap_road_points[j]))
+
+            mapping.append((i, heremap_names[road_prob.index(max(road_prob))]))
+
+        df = pd.DataFrame.from_records(mapping, columns=['sumo_road', 'here_map_road'])
+
         sumo_road = ['122516806', '143215059', '580921886#0', '28043853']
         here_map_road = ['2572+', '1835+', '2572-', '1836-']
+        df = pd.DataFrame({'sumo_road': sumo_road, 'here_map_road': here_map_road})
 
-        mapping = pd.DataFrame({'sumo_road': sumo_road, 'here_map_road': here_map_road})
-
-        return mapping
+        return df
 
     def average_vehicle_length(self, road):
         """
